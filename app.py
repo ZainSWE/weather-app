4435c08268b9a79b2cb091426f2364f505ec34c5
Zain Ahmad <165908202+ZainSWE@users.noreply.github.com>
2025-05-01 20:42:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import os
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -9,5 +10,10 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Ensure the app runs on port 5000, which is default for Flask
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # This block will only be executed if you run the script locally, and not in production
+    if os.environ.get('FLASK_ENV') == 'production':
+        # Use Waitress to serve the app in production
+        serve(app, host='0.0.0.0', port=5000)
+    else:
+        # Only use Flask's built-in server during local development
+        app.run(debug=True, host='0.0.0.0', port=5000)
